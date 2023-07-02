13b94c10795bce84944c47b901b5a08a010c4294
Jay Termulo <jetermulo@gmail.com>
2023-07-03 05:20:26 +0800

    Initial Mark Up - Header Navbar

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-// alert("Hello! I am an alert box!!");
+
+const exploreBtn = document.querySelector("#explore-btn");
+const logo = document.querySelector(".logo");
+const navList = document.querySelector("nav");
+
+exploreBtn.addEventListener("mouseover", showNavLis)
+function showNavLis(){
+    logo.style.visibility = "hidden";
+    exploreBtn.style.visibility = "hidden";
+    navList.style.visibility = "visible";
+    navList.classList.toggle('open');
+};
+
+
+navList.addEventListener("mouseleave", hideList)
+
+
+function hideList() {
+    exploreBtn.style.visibility = "visible";
+    exploreBtn.style.zIndex = "4"
+    logo.style.visibility = "visible";
+    navList.style.visibility = "hidden";
+    navList.classList.remove('open');
+    // navList.style.transition = "all .5s ease"
+    // navList.style.display = "none"
+
+};
